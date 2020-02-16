65dfcf8519e03892a20891df7bdcc65b01bc55ce
SaloniSS <saloni.shivdasani@utdallas.edu>
2020-02-16 06:24:00 -0600

    Sending found person

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,25 @@ import Person from './types/Person';
 
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
-
-const foundPerson: Person = {
-  role: "Back-End Development",
-  interests: [
-    "Data Science",
-    "Marketing",
-    "Back-End Development",
-    "Human Resources",
-    "Front-End Development"
-  ],
-  name: {
-    first: "Shirley",
-    last: "Cherry"
-  },
-  project: "Ignite",
-  email: "shirley.cherry@respec.com",
-  about: "Lorem esse elit irure officia eiusmod duis incididunt elit id dolor anim eiusmod pariatur. Sint sint cupidatat enim ex cillum ad. Nulla et sint occaecat veniam eu elit adipisicing ut. Amet dolore nostrud quis voluptate sint occaecat enim dolore adipisicing ipsum. Laborum nulla ut et irure. Occaecat ullamco ex reprehenderit qui reprehenderit aliquip magna cupidatat cupidatat pariatur exercitation excepteur."
-};
-
-
 type AppState = {
   toResult: boolean;
+  foundPerson: Person;
 };
 
 class App extends Component<{}, AppState>  {
   state: AppState = {
     toResult: false,
+    foundPerson: {
+      role: "",
+      interests: [],
+    name: {
+      first: "",
+      last: ""
+    },
+    project: "",
+    email: "",
+    about: ""
+    }
   };
   projects: string[] = [
     "ProjectX",
@@ -79,7 +71,7 @@ class App extends Component<{}, AppState>  {
             <Form projects={this.projects} roles={this.roles} interests={this.interests} onSubmit={ this.onSubmit } />
           </Route>
           <Route exact path="/done">
-            <Result person={foundPerson} />
+            <Result person={this.state.foundPerson} />
           </Route>
         </Switch>
       </Router>
@@ -125,7 +117,8 @@ class App extends Component<{}, AppState>  {
 
     console.log(match);
     this.setState({
-      toResult: true
+      toResult: true,
+      foundPerson: match 
     });
   };
 }
